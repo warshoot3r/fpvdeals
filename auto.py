3fd51fd554113fce090e06c06f8d758fb6ef0e30
james <james@james.local>
2024-03-25 16:37:24 +0000

    update python code to run out of home dir

--- auto.py
+++ auto.py
@@ -1,8 +1,3 @@
-import sys
-import os
-# Add the parent directory (project) to sys.path
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
 
 from modules.umt import UMTWebScraper
 from modules.db import UMTDatabase, umtbargainsproductobject
