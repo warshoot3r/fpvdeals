a751f358bdf2c869958588d65a9b3b2c262642f1
james <james@james.local>
2024-03-25 17:02:50 +0000

    update dockerfile

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-
-
 from modules.umt import UMTWebScraper
 from modules.db import UMTDatabase, umtbargainsproductobject
 from modules.telegram_bot import TelegramBot
 import credentials
 telegram = TelegramBot(api_token=credentials.api)
+
+
+
 
 
 UMTBargainsurl = 'https://www.unmannedtechshop.co.uk/product-category/parts-sensors/bargain-bin/'
