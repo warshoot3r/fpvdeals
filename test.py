1a49b81c1b7f9235134ada42aa8c5c902e946831
james <james@james.local>
2024-03-25 16:10:22 +0000

    add test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from modules.umt import UMTWebScraper
+from modules.db import UMTDatabase, umtbargainsproductobject
+from modules.telegram_bot import TelegramBot
+import credentials
+telegram = TelegramBot(api_token=credentials.api)
+
+
+UMTBargainsurl = 'https://www.unmannedtechshop.co.uk/product-category/parts-sensors/bargain-bin/'
+UMTBargains = UMTWebScraper(browser='chrome', headless=True, url=UMTBargainsurl)
+UMTBargainsDBTable = UMTDatabase(TableName="Bargains")
