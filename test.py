f714911b4f68f37ad758c6004214b94d8455747e
james <james@james.local>
2024-03-26 11:45:01 +0000

    remove credentials from test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from modules.umt import UMTWebScraper
 from modules.db import UMTDatabase, umtbargainsproductobject
 from modules.telegram_bot import TelegramBot
-import credentials
 telegram = TelegramBot(api_token=credentials.api)
 
 
