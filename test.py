1f67500499ac8952abbb5146a237abcefe5aee89
james <james@james.local>
2024-03-26 12:27:14 +0000

    update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from modules.umt import UMTWebScraper
 from modules.db import UMTDatabase, umtbargainsproductobject
 from modules.telegram_bot import TelegramBot
-telegram = TelegramBot(api_token=credentials.api)
+
 
 
 
