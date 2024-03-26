f1acdddac6ebec5b81e4a42ec724bf4a4127136f
james <james@james.local>
2024-03-26 15:06:39 +0000

    headless patch

--- modules/umt.py
+++ modules/umt.py
@@ -14,6 +14,25 @@ class UMTWebScraper:
     def initialize_driver(self):
         if self.browser.lower() == 'chrome':
             options = webdriver.ChromeOptions()
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1200")
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--start-maximized")
+            options.add_argument("--disable-default-apps")
+            options.add_argument("--disable-features=Translate")
+            options.add_argument("--disable-client-side-phishing-detection")
+            options.add_argument("--no-first-run")
+            options.add_argument("--incognito")
+            options.add_argument("--disable-component-extensions-with-background-pages")
+            options.add_argument("--disable-ipc-flooding-protection")
+            options.add_argument("--disable-hang-monitor")
+            options.add_argument("--disable-popup-blocking")
+            options.add_argument("--enable-automation")
+            options.add_argument("--disable-background-networking")
         elif self.browser.lower() == 'firefox':
             options = webdriver.FirefoxOptions()
         else:
